64c3474e485ffeaa43ff8d97823448d74359f48c
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-09-04 20:44:51 +0100

    Add files via upload

---
+++ PythonRevisionQuestions.py
@@ -0,0 +1,24 @@
+numbers = []
+while True:
+    value = input("Enter a number (or 'q' to quit): ")
+    if value.lower() == 'q':
+        break
+    else:
+        numbers.append(float(value))
+if len(numbers) == 0:
+    print("No numbers were entered.")
+mean = sum(numbers) / len(numbers)
+numbers.sort()
+n = len(numbers)
+if n % 2 == 0:  
+    median = (numbers[n//2 - 1] + numbers[n//2]) / 2
+else:           
+    median = numbers[n//2]
+
+
+
+print("Numbers entered:", numbers)
+print("Mean:", mean)
+print("Median:", median)
+print("Frequency:", )
+print("Mode:", )

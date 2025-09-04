1af3e495aade6ee8bd024834da4ca5761f516a29
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-09-04 20:57:25 +0100

    PythonRevisionQuestions.py

--- PythonRevisionQuestions.py
+++ PythonRevisionQuestions.py
@@ -1,25 +1,27 @@
+
+# Mean = average value.
+# Median = middle value (after sorting).
+# Frequency = how many times each number appears.
+# Mode = most common number(s).
 numbers = []
 while True:
     value = input("Enter a number (or 'q' to quit): ")
     if value.lower() == 'q':
         break
     else:
-        numbers.append(float(value))
+        numbers.append(float(value)) #Adding the user input to the list "numbers"
 if len(numbers) == 0:
     print("No numbers were entered.")
-mean = sum(numbers) / len(numbers)
-numbers.sort()
+mean = sum(numbers) / len(numbers) #Diving the sum of "numbers" by the lenght of "numbers" to get the average
+numbers.sort() #Sorting the numbers in order from smallest to largest
 n = len(numbers)
-if n % 2 == 0:  
-    median = (numbers[n//2 - 1] + numbers[n//2]) / 2
-else:           
-    median = numbers[n//2]
-
+    
 #I'm a bit stuck, not sure how to get frequency
+
+
 
 print("Numbers entered:", numbers)
 print("Mean:", mean)
-print("Median:", median)
+print("Median:", )
 print("Frequency:", )
-
 print("Mode:", )

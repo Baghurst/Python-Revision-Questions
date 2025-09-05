4cb92fd04ab9da641cfd1065ebb0d104d668baef
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-09-05 10:46:02 +0100

    Update PythonRevisionQuestions.py

--- PythonRevisionQuestions.py
+++ PythonRevisionQuestions.py
@@ -25,3 +25,13 @@ print("Mean:", mean)
 print("Median:", )
 print("Frequency:", )
 print("Mode:", )
+
+
+
+username = input("What's your name?: ")
+surname = input("What's your surname?: ")
+print(f"Hello {username} {surname}!")
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+total = num1 + num2
+print(total)

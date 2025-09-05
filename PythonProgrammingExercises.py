0ee612399ba74de070fd273585641abf816dadad
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-09-05 16:42:36 +0100

    Add files via upload

---
+++ PythonProgrammingExercises.py
@@ -0,0 +1,7 @@
+username = input("What's your name?: ")
+surname = input("What's your surname?: ")
+print(f"Hello {username} {surname}!")
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+total = num1 + num2
+print(total)
